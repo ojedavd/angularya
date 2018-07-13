f48b6ea9e954f9c91e9cba4eb9c46055cbd82b24
Victor Ojeda <ojedavd@hotmail.com>
2018-07-13 09:57:03 -0300

    Disparo de eventos de la componente hija a la componente padre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  valor1: number;
-  valor2: number;
-  valor3: number;
-  resultado: string;
-  constructor() {
-    this.valor1 = this.retornarAleatorio();
-    this.valor2 = this.retornarAleatorio();
-    this.valor3 = this.retornarAleatorio();
-  }
+  mensaje='';
 
-  retornarAleatorio() {
-    return Math.trunc(Math.random() * 6) + 1;
-  }
-
-  tirar() {
-    this.valor1 = this.retornarAleatorio();
-    this.valor2 = this.retornarAleatorio();
-    this.valor3 = this.retornarAleatorio();
-    if (this.valor1==this.valor2 && this.valor1==this.valor3)    
-      this.resultado='Ganó';
-    else
-      this.resultado='Perdió';
+  actualizar(t) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DadoComponent } from './dado/dado.component';
+import { CronometroComponent } from './cronometro/cronometro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DadoComponent
+    DadoComponent,
+    CronometroComponent
   ],
   imports: [
     BrowserModule
