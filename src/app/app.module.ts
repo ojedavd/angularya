2ceac31f3ba8beb22258bc1ddf4aed006d76bb45
Victor Ojeda <ojedavd@hotmail.com>
2018-07-13 16:02:24 -0300

    Servicios: recuperar datos desde un servidor web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { ArticulosService } from './articulos.service';
-
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule    
   ],
   providers: [ArticulosService],
   bootstrap: [AppComponent]
